916660e53c02e087417d6ee368f5670e94231069
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-08-14 14:36:43 +0200

    Add get data function

---
+++ src/bio2bel_mirtarbase/run.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+DATA_URL = 'http://mirtarbase.mbc.nctu.edu.tw/cache/download/6.1/miRTarBase_MTI.xlsx'
+
+
+def get_data():
+    """Gets miRTarBase Interactions table
+
+    :rtype: pandas.DataFrame
+    """
+    return pd.read_excel(DATA_URL)
