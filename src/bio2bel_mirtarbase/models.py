248e16ab31db07d691f4def3de59a7e0f0e907e8
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-05 16:10:25 +0100

    Update text and nomenclature

--- src/bio2bel_mirtarbase/models.py
+++ src/bio2bel_mirtarbase/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from pybel.constants import FUNCTION, MIRNA, NAME, NAMESPACE, RNA
+from pybel.constants import FUNCTION, IDENTIFIER, MIRNA, NAME, NAMESPACE, RNA
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -8,15 +8,22 @@ from sqlalchemy.orm import relationship
 ENTREZ_GENE_ID = 'EGID'
 MIRTARBASE_ID = 'MIRTARBASE'
 
+MIRTARBASE_PREFIX = 'mirtarbase'
+MIRNA_TABLE_NAME = '{}_mirna'.format(MIRTARBASE_PREFIX)
+TARGET_TABLE_NAME = '{}_target'.format(MIRTARBASE_PREFIX)
+EVIDENCE_TABLE_NAME = '{}_evidence'.format(MIRTARBASE_PREFIX)
+INTERACTION_TABLE_NAME = '{}_interaction'.format(MIRTARBASE_PREFIX)
+
 # create base class
 Base = declarative_base()
 
 
 class Mirna(Base):
     """Create mirna table that stores information about the miRNA"""
-    __tablename__ = "mirna"
+    __tablename__ = MIRNA_TABLE_NAME
 
     id = Column(Integer, primary_key=True)
+
     mirtarbase_id = Column(String, nullable=False, unique=True, doc="miRTarBase ID")
     mir_name = Column(String, nullable=False, doc="miRNA name")
     species = Column(String, nullable=False, doc="Species associated with miRNA")
@@ -29,15 +36,17 @@ class Mirna(Base):
         return {
             FUNCTION: MIRNA,
             NAMESPACE: MIRTARBASE_ID,
-            NAME: self.mirtarbase_id
+            IDENTIFIER: self.mirtarbase_id,
+            NAME: self.mir_name
         }
 
 
 class Target(Base):
     """Build target table, which stores information about the target gene"""
-    __tablename__ = "target"
+    __tablename__ = TARGET_TABLE_NAME
 
     id = Column(Integer, primary_key=True)
+
     target_gene = Column(String, nullable=False, doc="Target gene name")
     entrez_id = Column(String, nullable=False, unique=True, doc="Target gene Entrez ID")
     species = Column(String, nullable=False, doc="Species associated with target gene")
@@ -52,7 +61,8 @@ class Target(Base):
         return {
             FUNCTION: RNA,
             NAMESPACE: ENTREZ_GENE_ID,
-            NAME: self.entrez_id
+            IDENTIFIER: self.entrez_id,
+            NAME: self.target_gene,
         }
 
     def serialize_to_hgnc_node(self):
@@ -63,10 +73,15 @@ class Target(Base):
         return {
             FUNCTION: RNA,
             NAMESPACE: 'HGNC',
+            IDENTIFIER: self.hgnc_id,
             NAME: self.hgnc_symbol
         }
 
     def to_json(self):
+        """Returns this object as JSON
+
+        :rtype: dict
+        """
         return {
             'id': self.id,
             'species': self.species,
@@ -83,9 +98,10 @@ class Target(Base):
 
 class Evidence(Base):
     """Build Evidence table used to store MTI's and their evidence"""
-    __tablename__ = "evidence"
+    __tablename__ = EVIDENCE_TABLE_NAME
 
     id = Column(Integer, primary_key=True)
+
     experiment = Column(String, nullable=False,
                         doc="Experiments made to find miRNA - target interaction. E.g. 'Luciferase reporter assay//qRT-PCR//Western blot'")
     support = Column(String, nullable=False,
@@ -95,14 +111,15 @@ class Evidence(Base):
 
 class Interaction(Base):
     """Build Interaction table used to store miRNA and target relations"""
-    __tablename__ = "interaction"
+    __tablename__ = INTERACTION_TABLE_NAME
 
     id = Column(Integer, primary_key=True)
-    mirna_id = Column(Integer, ForeignKey("mirna.id"))
+
+    mirna_id = Column(Integer, ForeignKey("{}.id".format(MIRNA_TABLE_NAME)))
     mirna = relationship("Mirna", backref="interactions")
 
-    target_id = Column(Integer, ForeignKey("target.id"))
+    target_id = Column(Integer, ForeignKey("{}.id".format(TARGET_TABLE_NAME)))
     target = relationship("Target", backref="interactions")
 
-    evidence_id = Column(Integer, ForeignKey("evidence.id"))
+    evidence_id = Column(Integer, ForeignKey("{}.id".format(EVIDENCE_TABLE_NAME)))
     evidence = relationship("Evidence", backref="interactions")
