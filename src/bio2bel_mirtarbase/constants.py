e142232b04cba4572e74ea17d60e8a17aa6f8ed3
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-08 11:15:42 +0100

    Update constants

--- src/bio2bel_mirtarbase/constants.py
+++ src/bio2bel_mirtarbase/constants.py
@@ -5,12 +5,11 @@ import os
 #: Data source
 DATA_URL = 'http://mirtarbase.mbc.nctu.edu.tw/cache/download/6.1/miRTarBase_MTI.xlsx'
 
-MIRTARBASE_DATA_DIR = os.path.join(os.path.expanduser('~'), '.pymirtarbase')
+MIRTARBASE_DATA_DIR = os.path.join(os.path.expanduser('~'), '.pybel', 'bio2bel', 'mirtarbase')
+os.makedirs(MIRTARBASE_DATA_DIR, exist_ok=True)
 
-if not os.path.exists(MIRTARBASE_DATA_DIR):
-    os.makedirs(MIRTARBASE_DATA_DIR)
-
-MIRTARBASE_DATABASE_NAME = 'miRTarBase.db'
-MIRTARBASE_SQLITE_PATH = 'sqlite:///' + os.path.join(MIRTARBASE_DATA_DIR, MIRTARBASE_DATABASE_NAME)
+MIRTARBASE_DATABASE_NAME = 'mirtarbase.db'
+MIRTARBASE_DATABASE_PATH = os.path.join(MIRTARBASE_DATA_DIR, MIRTARBASE_DATABASE_NAME)
+MIRTARBASE_SQLITE_PATH = 'sqlite:///' + MIRTARBASE_DATABASE_PATH
 
 MIRTARBASE_CONFIG_FILE_PATH = os.path.join(MIRTARBASE_DATA_DIR, 'config.ini')
