387aa4a92b83e47fe7f66409825c7a096e5efdc9
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-02-18 22:00:37 +0100

    Update documentation

--- src/bio2bel_mirtarbase/web.py
+++ src/bio2bel_mirtarbase/web.py
@@ -1,24 +1,47 @@
 # -*- coding: utf-8 -*-
 
+"""This module builds a :mod:`Flask` application for interacting with the underlying database. When installing,
+use the web extra like:
+
+.. source-code:: sh
+
+    pip install bio2bel_mirtarbase[web]
+"""
+
 import flask_admin
 from flask import Flask
 from flask_admin.contrib.sqla import ModelView
 
-from .manager import Manager
-from .models import *
+from bio2bel_mirtarbase.manager import Manager
+from bio2bel_mirtarbase.models import *
 
 
 def add_admin(app, session, **kwargs):
+    """Adds a Flask Admin interface to an application
+
+    :param flask.Flask app:
+    :param session:
+    :param kwargs:
+    :rtype: flask_admin.Admin
+    """
     admin = flask_admin.Admin(app, **kwargs)
+
     admin.add_view(ModelView(Interaction, session))
     admin.add_view(ModelView(Mirna, session))
     admin.add_view(ModelView(Target, session))
     admin.add_view(ModelView(Evidence, session))
     admin.add_view(ModelView(Species, session))
+
     return admin
 
 
 def get_app(connection=None, url=None):
+    """Creates a Flask application
+
+    :type connection: Optional[str or bio2bel_mirtarbase.Manager]
+    :type url: Optional[str]
+    :rtype: flask.Flask
+    """
     app = Flask(__name__)
     manager = Manager.ensure(connection=connection)
     add_admin(app, manager.session, url=url)
@@ -26,5 +49,5 @@ def get_app(connection=None, url=None):
 
 
 if __name__ == '__main__':
-    app = get_app()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app_ = get_app()
+    app_.run(debug=True, host='0.0.0.0', port=5000)
