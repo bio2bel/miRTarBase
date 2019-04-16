6113683d94b63ac67e39cb95905601a402b6bc6a
Charles Tapley Hoyt <cthoyt@wlan-182.scai.fraunhofer.de>
2019-04-16 16:07:14 +0200

    Bump version: 0.2.1 → 0.2.2-dev

--- src/bio2bel_mirtarbase/__init__.py
+++ src/bio2bel_mirtarbase/__init__.py
@@ -4,7 +4,7 @@
 
 from .manager import Manager  # noqa: F401
 
-__version__ = '0.2.1'
+__version__ = '0.2.2-dev'
 
 __title__ = 'bio2bel_mirtarbase'
 __description__ = "A package for converting miRTarBase to BEL"
